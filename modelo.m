8ee8606a029cca687b13f28aeb5613dda278df29
Jose <Jose@10.1.0.5>
2016-12-05 18:06:06 +0100

    modelo acabado

--- modelo.m
+++ modelo.m
@@ -1,11 +1,13 @@
 %Sistema de EDO's
-%En este documento queda definido el sistema de Ecuaciones Diferenciales
-%que nos sirve como modelo matemático (sistema dinámico) de las poblaciones
-%que queremos estudiar.
-function [dS,dI,dZ,dR]=fun(S,I,Z,R,a,b,c,d)
-    dS=-b*S*Z;          %Poblacion Sana
-    dI=b*S*Z-d*I;       %Población Infectada
-    dZ=d*I+c*R-a*S*Z;   %Población Zombie
-    dR=a*S*Z-c*R;       %Población Muerta Zombificable
+%Aquí está el sistema de ecuaciones diferenciales del sistema dinámico 
+%que modela nuestro universo. Las cuatro ecuaciones representan
+%respectivamente la variación de las cuatro poblaciones (Humanos Sanos,
+%Humanos Infectados, Zombies, Muertos) con respecto de la variable tiempo.
+
+function [dS,dI,dZ,dR]=modelo(S,I,Z,R,a,b,c,d)
+    dS=-b*S*Z;          %Poblacion sana
+    dI=b*S*Z-d*I;       %Población infectada
+    dZ=d*I+c*R-a*S*Z;   %Población zombie
+    dR=a*S*Z-c*R;       %Población muerta susceptible de zombificación.
 end
 
