8485cff7dce605ec4f00da5312e0f90f91c760a9
Jose Antonio García del Saz <Jose@stadium.local>
2016-12-05 18:29:52 +0100

    apartado1AM

--- apartado1_AdamsMoulton.m
+++ apartado1_AdamsMoulton.m
@@ -1,28 +1,35 @@
+%Introducimos los valores iniciales del PVI que aparecen en el enunciado
+%para las poblaciones
 S0 = 500;
 I0 = 0;
 Z0 = 2;
 R0 = 0;
+%y para los parametros de crecimiento/extincion
 a = 0.005;
 b = 0.0095;
 c = 0.0001;
 d = 3;
+%Queremos que haya 1000 nodos y 10 días
 ini=0;
 fin=10;
+pasos=1000;
 
-pasos=500;
+%Llamamos al método de Adams-Moulton pasando los argumentos en orden
+[S,I,Z,R,t] = adamsMoulton(@modelo,pasos,ini,fin,S0,I0,Z0,R0,a,b,c,d); 
 
+%Construimos una gráfica, donde mostraremos las cuatro poblaciones y además
+%el valor sum, que es el cómputo total de individuos
+sum=S+I+Z+R;
 figure(1);
 hold all;
-
-[S,I,Z,R,t] = adamsMoulton(@fun,pasos,ini,fin,S0,I0,Z0,R0,a,b,c,d); %Metodo de adams
-n=S+I+Z+R;
 plot(S);
 plot(I);
 plot(Z);
 plot(R);
-plot(n);
-title('Solucion numerica');
-xlabel('x');
-ylabel('y');
+plot(sum);
+%Etiquetamos la gráfica y listo
+title('Apartado 1.b: Método de Adams-Moulton (2 pasos)');
+xlabel('Nodos');
+ylabel('Cantidad');
 legend('Humanos','Infectados','Zombies','Muertos','Suma');
 hold all;
